0c83c6fe493b791cbaf7f9f8ab322eec3a3932fb
Michaël Le Marec <lemarec@gmail.com>
2022-04-16 17:20:36 +0200

    Update data

--- commands.sh
+++ commands.sh
@@ -59,7 +59,21 @@ show dbs
 use sample_training
 db.zips.find().pretty()
 db.zips.find({"zip": "12534"})
+
+db.zips.find({ "city": "HUDSON" }).count()
+
 db.zips.updateMany({ "city": "HUDSON" }, { "$inc": { "pop": 10 }})
 # "$inc" => increment
 
-db.zips.updateOne({})
+db.zips.updateOne({ "zip": "12534" }, { "$set": { "pop": 17630 } })
+
+db.grades.find()
+db.grades.find({ "student_id": 151})
+db.grades.find({ "student_id": 151, "class_id": 339 }).pretty()
+db.grades.find({ "student_id": 250, "class_id": 339 })
+
+db.grades.updateOne({ "student_id": 250, "class_id": 339 },
+                    { "$push": { "scores": { "type": "extra credit",
+                                             "score": 100 }
+                                }
+                     })
